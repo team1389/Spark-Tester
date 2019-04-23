f2af49145146c8f687fd1d53b7894e924835f036
kenneth-miura <kenneth.t.miura@gmail.com>
2019-04-23 19:58:31 -0400

    untested gui implementation

--- gui.py
+++ gui.py
@@ -1,7 +1,16 @@
 import PySimpleGUI as sg
 
+# Network Tables var setup
+network_table_spark_prefix = "Spark"
+network_table_server = "roborio-1389-frc.local"
+network_table_table_name = "Spark-Tester-Table"
+
+table = get_network_table()
+sparks = get_spark_entries(table)
+spark_entry_ids = get_spark_ids(table)
+
 layout = [
-    [sg.Listbox(values=['Spark 1', 'Spark 2', 'Spark 3'], size=(
+    [sg.Listbox(values=spark_entry_ids, size=(
         30, 6), select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE)],
     [sg.Slider(range=(-1, 1), default_value=0, size=(20, 15),
                orientation='horizontal', resolution=0.1, change_submits=True)],
@@ -18,12 +27,19 @@ while True:
 window.Close()
 
 
-def get_all_sparks():
-    # TODO:Find some way of checking network tables for the port of each spark thats initialized,
-    # make list string of 'Spark + port', return it
-    pass
+def get_network_table():
+    NetworkTables.initialize(server=network_table_server)
+    return NetworkTables.getTable(network_table_table_name)
 
 
-def set_motors(percent_to_apply):
-    # Send motor vals to apply to robot side through network tables
-    pass
+def get_spark_entries(table):
+    return table.getEntries(network_table_spark_prefix)
+
+
+def get_spark_ids(table):
+    return map(lambda entry: entry.getName(), table.getEntries(network_table_spark_prefix))
+
+
+def set_motors(sparks_ids, table, percent_to_apply):
+    map(lambda spark_id: table.getEntry(
+        spark_id).setDouble(percent_to_apply), sparks_ids)
